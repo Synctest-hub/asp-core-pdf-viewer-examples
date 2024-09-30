bf0569dd02a4a574341ab2ef65561f78974c9987
VijayalakshmiGSF4561 <vijayalakshmi.govindan@syncfusion.com>
2024-09-30 10:00:15 +0530

    :Removed comment lines

--- How to/Add Handwritten Signature Programmatically/PDFViewerSample/PDFViewerSample/Pages/Index.cshtml.cs
+++ How to/Add Handwritten Signature Programmatically/PDFViewerSample/PDFViewerSample/Pages/Index.cshtml.cs
@@ -21,11 +21,14 @@ namespace PDFViewerSample.Pages
             _cache = cache;
         }
 
-        public IActionResult OnPostLoad([FromBody] jsonObjects responseData)
+        [HttpPost("Load")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/Load")]
+        //Post action for Loading the PDF documents  
+        public IActionResult OnPostLoad([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
             MemoryStream stream = new MemoryStream();
-            var jsonObject = JsonConverterstring(responseData);
             object jsonResult = new object();
             if (jsonObject != null && jsonObject.ContainsKey("document"))
             {
@@ -60,27 +63,23 @@ namespace PDFViewerSample.Pages
             return Content(JsonConvert.SerializeObject(jsonResult));
         }
 
-        public Dictionary<string, string> JsonConverterstring(jsonObjects results)
-        {
-            Dictionary<string, object> resultObjects = new Dictionary<string, object>();
-            resultObjects = results.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                .ToDictionary(prop => prop.Name, prop => prop.GetValue(results, null));
-            var emptyObjects = (from kv in resultObjects
-                                where kv.Value != null
-                                select kv).ToDictionary(kv => kv.Key, kv => kv.Value);
-            Dictionary<string, string> jsonResult = emptyObjects.ToDictionary(k => k.Key, k => k.Value.ToString());
-            return jsonResult;
-        }
-
+        [AcceptVerbs("Post")]
+        [HttpPost("RenderPdfPages")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/RenderPdfPages")]
         //Post action for processing the PDF documents.
-        public IActionResult OnPostRenderPdfPages([FromBody] jsonObjects responseData)
+        public IActionResult OnPostRenderPdfPages([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             object jsonResult = pdfviewer.GetPage(jsonObject);
             return Content(JsonConvert.SerializeObject(jsonResult));
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("RenderPdfPages")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/RenderPdfPages")]
+        //Post action for processing the PDF documents
          public IActionResult RenderPdfTexts([FromBody] Dictionary<string, string> jsonObject)
         {
             //Initialize the PDF Viewer object with memory cache object
@@ -89,6 +88,10 @@ namespace PDFViewerSample.Pages
             return Content(JsonConvert.SerializeObject(jsonResult));
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("ValidatePassword")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/ValidatePassword")]
          public IActionResult ValidatePassword([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
@@ -138,56 +141,74 @@ namespace PDFViewerSample.Pages
         }
 
 
-        //Post action for unloading and disposing the PDF document resources
-        public IActionResult OnPostUnload([FromBody] jsonObjects responseData)
+        [AcceptVerbs("Post")]
+        [HttpPost("Unload")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/Unload")]
+        //Post action for unloading and disposing the PDF document resources  
+        public IActionResult OnPostUnload([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             pdfviewer.ClearCache(jsonObject);
             return this.Content("Document cache is cleared");
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("RenderThumbnailImages")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/RenderThumbnailImages")]
         //Post action for rendering the ThumbnailImages
-        public IActionResult OnPostRenderThumbnailImages([FromBody] jsonObjects responseData)
+        public IActionResult OnPostRenderThumbnailImages([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             object result = pdfviewer.GetThumbnailImages(jsonObject);
             return Content(JsonConvert.SerializeObject(result));
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("Bookmarks")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/Bookmarks")]
         //Post action for processing the bookmarks from the PDF documents
-        public IActionResult OnPostBookmarks([FromBody] jsonObjects responseData)
+        public IActionResult OnPostBookmarks([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             object jsonResult = pdfviewer.GetBookmarks(jsonObject);
             return Content(JsonConvert.SerializeObject(jsonResult));
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("RenderAnnotationComments")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/RenderAnnotationComments")]
         //Post action for rendering the annotation comments
-        public IActionResult OnPostRenderAnnotationComments([FromBody] jsonObjects responseData)
+        public IActionResult OnPostRenderAnnotationComments([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             object jsonResult = pdfviewer.GetAnnotationComments(jsonObject);
             return Content(JsonConvert.SerializeObject(jsonResult));
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("ExportAnnotations")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/ExportAnnotations")]
         //Post action for exporting the annotations
-        public IActionResult OnPostExportAnnotations([FromBody] jsonObjects responseData)
+        public IActionResult OnPostExportAnnotations([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             string jsonResult = pdfviewer.ExportAnnotation(jsonObject);
             return Content(jsonResult);
         }
 
+        [AcceptVerbs("Post")]
+        [HttpPost("ImportAnnotations")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/ImportAnnotations")]
         //Post action for importing the annotations
-        public IActionResult OnPostImportAnnotations([FromBody] jsonObjects responseData)
+        public IActionResult OnPostImportAnnotations([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             string jsonResult = string.Empty;
             object JsonResult;
             if (jsonObject != null && jsonObject.ContainsKey("fileName"))
@@ -229,20 +250,24 @@ namespace PDFViewerSample.Pages
             return Content(jsonResult);
         }
 
+        [HttpPost("Download")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/Download")]
         //Post action for downloading the PDF documents
-        public IActionResult OnPostDownload([FromBody] jsonObjects responseData)
+        public IActionResult OnPostDownload([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             string documentBase = pdfviewer.GetDocumentAsBase64(jsonObject);
             return Content(documentBase);
         }
 
+        [HttpPost("PrintImages")]
+        [Microsoft.AspNetCore.Cors.EnableCors("MyPolicy")]
+        [Route("[controller]/PrintImages")]
         //Post action for printing the PDF documents
-        public IActionResult OnPostPrintImages([FromBody] jsonObjects responseData)
+        public IActionResult OnPostPrintImages([FromBody] Dictionary<string, string> jsonObject)
         {
             PdfRenderer pdfviewer = new PdfRenderer(_cache);
-            var jsonObject = JsonConverterstring(responseData);
             object pageImage = pdfviewer.GetPrintImage(jsonObject);
             return Content(JsonConvert.SerializeObject(pageImage));
         }
@@ -265,59 +290,5 @@ namespace PDFViewerSample.Pages
             }
             return documentPath;
         }
-    }
-
-    public class jsonObjects
-    {
-        public string document { get; set; }
-        public string password { get; set; }
-        public string zoomFactor { get; set; }
-        public string isFileName { get; set; }
-        public string xCoordinate { get; set; }
-        public string yCoordinate { get; set; }
-        public string pageNumber { get; set; }
-        public string documentId { get; set; }
-        public string hashId { get; set; }
-        public string sizeX { get; set; }
-        public string sizeY { get; set; }
-        public string startPage { get; set; }
-        public string endPage { get; set; }
-        public string stampAnnotations { get; set; }
-        public string textMarkupAnnotations { get; set; }
-        public string stickyNotesAnnotation { get; set; }
-        public string shapeAnnotations { get; set; }
-        public string measureShapeAnnotations { get; set; }
-        public string action { get; set; }
-        public string pageStartIndex { get; set; }
-        public string pageEndIndex { get; set; }
-        public string fileName { get; set; }
-        public string elementId { get; set; }
-        public string pdfAnnotation { get; set; }
-        public string importPageList { get; set; }
-        public string uniqueId { get; set; }
-        public string data { get; set; }
-        public string viewPortWidth { get; set; }
-        public string viewPortHeight { get; set; }
-        public string tilecount { get; set; }
-        public bool isCompletePageSizeNotReceived { get; set; }
-        public string freeTextAnnotation { get; set; }
-        public string signatureData { get; set; }
-        public string fieldsData { get; set; }
-        public string FormDesigner { get; set; }
-        public string inkSignatureData { get; set; }
-        public bool hideEmptyDigitalSignatureFields { get; set; }
-        public bool showDigitalSignatureAppearance { get; set; }
-        public bool digitalSignaturePresent { get; set; }
-        public string tileXCount { get; set; }
-        public string tileYCount { get; set; }
-        public string digitalSignaturePageList { get; set; }
-        public string annotationCollection { get; set; }
-        public string annotationsPageList { get; set; }
-        public string formFieldsPageList { get; set; }
-        public bool isAnnotationsExist { get; set; }
-        public bool isFormFieldAnnotationsExist { get; set; }
-        public string documentLiveCount { get; set; }
-        public string annotationDataFormat { get; set; }
-        public string importedData { get; set; }
     }
 }
