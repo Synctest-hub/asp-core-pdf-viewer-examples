16795775838e7abd812f8a7ac3c533c19300df0f
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-02-05 10:59:42 +0530

    931795: Removed PDF Viewer Web API code in controllers file

--- How to/Load PDF to Server Side PDF Viewer/Load-PDF-to-Server-Side-PDF-Viewer/Controllers/PdfViewerController.cs
+++ How to/Load PDF to Server Side PDF Viewer/Load-PDF-to-Server-Side-PDF-Viewer/Controllers/PdfViewerController.cs
@@ -255,19 +255,6 @@ namespace CoreSample.Controllers
             Console.WriteLine(documentPath);
             return documentPath;
         }
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
     }
 }
