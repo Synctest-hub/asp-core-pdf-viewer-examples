cdc02380fd9ca62bb8bb7d6cb6ca150db2b48206
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-08-01 18:25:30 +0530

    897552: Committed in wrong branch by mistake

--- How to/Create User Based eSign PDF Forms/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ESigningSimpleSample.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
